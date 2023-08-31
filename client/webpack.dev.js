6fe9d64b792c90da992571bf19f2ba6c18e85b5e
Denys Savytskyi <deonissavytsk@gmail.com>
2023-08-31 23:17:58 +0200

    add frontend config

---
+++ client/webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '.'),
+    },
+    client: {
+      overlay: false,
+    },
+    hot: true,
+    port: 5555,
+  },
+});
