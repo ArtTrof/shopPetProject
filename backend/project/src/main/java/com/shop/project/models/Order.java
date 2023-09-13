9ae716f9bf64cd9acb6bf47ef4b1d1894fa5c923
Artem Trofymenko <118562434+ArtTrof@users.noreply.github.com>
2023-09-13 21:23:39 +0200

    Implementation of editing quantity of the cart item (#5)

--- backend/project/src/main/java/com/shop/project/models/Order.java
+++ backend/project/src/main/java/com/shop/project/models/Order.java
@@ -5,11 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.w3c.dom.stylesheets.LinkStyle;
 
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- backend/project/src/main/java/com/shop/project/repository/CartItemRepo.java
+++ backend/project/src/main/java/com/shop/project/repository/CartItemRepo.java
@@ -4,6 +4,7 @@ import com.shop.project.models.CartItem;
 import com.shop.project.models.Customer;
 import com.shop.project.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,7 @@ public interface CartItemRepo extends JpaRepository<CartItem, Long> {
     Optional<List<CartItem>> findByCustomer_Id(Long customerId);
 
     Optional<CartItem> findCartByProductAndCustomer(Product product, Customer customer);
+
+    Optional<CartItem> findCartItemByProduct_IdAndCustomer_Id(Long productId, Long customerId);
+
 }
