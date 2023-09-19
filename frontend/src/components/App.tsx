88bcf0e8a4121273758d334ce18fe0340dedb1c9
Denys Savytskyi <deonissavytsk@gmail.com>
2023-09-19 04:09:23 +0200

    add routing

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,31 +1,19 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-import Header from "./Header";
-import Footer from "./Footer/Footer";
-import ContactUsWidget from "./ContactUsWidget";
-import SingUp from '../pages/SingUp/SingUp';
+import { Route, Routes } from 'react-router-dom';
+import { NotFound, SignUp } from '../pages';
+import Header from './Header';
+import Footer from './Footer';
+import ContactUsWidget from './ContactUsWidget';
 
 export default () =>  {
-
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#fff',
-      },
-      secondary: {
-        main: '#000',
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <div className='min-h-screen m-0 p-0'>
-        <ContactUsWidget />
-        <Header />
-            <SingUp/>
-        <Footer />
-      </div>
-    </ThemeProvider>
-  );
+    <>
+      <ContactUsWidget />
+      <Header />
+      <Routes>
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+    );
 };
