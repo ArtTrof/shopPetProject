c31fe1e5793b5178f00671ebd9d63ac5b1d75d4b
ArtTrof <randallsng@gmail.com>
2023-09-20 19:45:22 +0200

    import optimisation

--- backend/project/src/main/java/com/shop/project/repository/CartItemRepo.java
+++ backend/project/src/main/java/com/shop/project/repository/CartItemRepo.java
@@ -4,7 +4,6 @@ import com.shop.project.models.CartItem;
 import com.shop.project.models.Customer;
 import com.shop.project.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- backend/project/src/main/java/com/shop/project/repository/OrderItemRepo.java
+++ backend/project/src/main/java/com/shop/project/repository/OrderItemRepo.java
@@ -1,11 +1,8 @@
 package com.shop.project.repository;
 
-import com.shop.project.models.Order;
 import com.shop.project.models.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
