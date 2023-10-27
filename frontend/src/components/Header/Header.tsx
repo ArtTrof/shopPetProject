3807dc1044561375113a5181e5ed440086ad5500
SERHII KUZUB <kuzub2431@gmail.com>
2023-10-27 19:48:27 +0300

    little upd styles

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -14,21 +14,28 @@ export default () => {
 		setActiveMenu(index)
 	}
 
-    const navMenu = ['Laptops', 'Phone', 'PC', 'All Other'];
+    const navMenu = ['Laptops', 'Decktop PCs', 'Phone', 'All Other'];
 
     return (
         <div className='w-full shadow-md'>
             <div className="flex items-center max-w-screen-2xl justify-between mx-auto h-16">
                 <div className='flex items-center'>
                     <img
-                        onClick={() => navigate('/')}
+                        onClick={() => {
+                            navigate('/');
+                            setActiveMenu(-1);
+                        }}
                         src="/logo.png"
                         alt="Logo" className='h-11 w-auto cursor-pointer'
                     />
                     { navMenu.map((value, index) => (
                             <Typography key={index}
                                 onClick={() => onActiveMenu(index)}
-                                className={activeMenu === index ? 'text-sky-500 underline cursor-pointer !ml-4' : 'text-black hover:text-sky-500 cursor-pointer !ml-4'}>{value}
+                                className={activeMenu === index 
+                                ? 'text-sm text-white bg-sky-600 rounded-2xl font-semibold cursor-pointer py-2 px-4 !ml-4' 
+                                : 'hover:text-white hover:bg-sky-600 hover:rounded-2xl text-sm text-black font-semibold cursor-pointer py-2 px-4 !ml-4 transition-all duration-300'}
+                            >
+                                {value}
                             </Typography>
                         )
                     )}
